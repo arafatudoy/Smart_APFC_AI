3d992d092cc28a10c0b3faced1671820fbb3ca39
arafatudoy <arafat.udoy4409@gmail.com>
2026-09-22 15:15:20 +0600

    Connect dashboard with cloud API

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -9,7 +9,7 @@ import time
 # SETTINGS
 # =====================================================
 
-SERVER_URL = "http://127.0.0.1:5000/api/data"
+SERVER_URL = "https://smart-apfc-api.onrender.com/api/data"
 
 
 
